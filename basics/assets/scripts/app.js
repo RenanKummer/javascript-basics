faab9fa2479976bc91ef90e2756eb7ef9457ef70
Renan Kummer <dev.renankummer@outlook.com>
2021-01-08 23:00:30 -0300

    :sparkles: Save history of calculations
    Work with arrays and objects to save and log all the calculations. Fix
    conditional logic to confirm if user entered a valid number.

--- basics/assets/scripts/app.js
+++ basics/assets/scripts/app.js
@@ -1,10 +1,11 @@
 const defaultResult = 0;
 let acumulator = defaultResult;
+let calculatorHistory = [];
 
 function getUserInputAsInt() {
   const numberInput = parseInt(userInput.value);
 
-  if (!isNaN(numberInput)) {
+  if (typeof numberInput === 'number' && !isNaN(numberInput)) {
     return numberInput;
   }
 
@@ -20,35 +21,79 @@ function describeCalculation(operator, num1, num2) {
   return '';
 }
 
+function logCalculatorHistory(
+  operator,
+  inputNum,
+  previousTotal,
+  newTotal,
+  description
+) {
+  const logEntry = {
+    operator: operator,
+    number: inputNum,
+    previousTotal: previousTotal,
+    newTotal: newTotal,
+    description: description
+  };
+
+  calculatorHistory.push(logEntry);
+  console.log(calculatorHistory);
+}
+
 function add() {
   const numberToAdd = getUserInputAsInt();
+  const previousTotal = acumulator;
   const description = describeCalculation('+', acumulator, numberToAdd);
 
   acumulator += numberToAdd;
 
   outputResult(acumulator, description);
+  logCalculatorHistory(
+    '+',
+    numberToAdd,
+    previousTotal,
+    acumulator,
+    description
+  );
 }
 
 function subtract() {
   const numberToSubtract = getUserInputAsInt();
+  const previousTotal = acumulator;
   const description = describeCalculation('-', acumulator, numberToSubtract);
 
   acumulator -= numberToSubtract;
 
   outputResult(acumulator, description);
+  logCalculatorHistory(
+    '-',
+    numberToSubtract,
+    previousTotal,
+    acumulator,
+    description
+  );
 }
 
 function multiply() {
   const numberToMultiply = getUserInputAsInt();
+  const previousTotal = acumulator;
   const description = describeCalculation('*', acumulator, numberToMultiply);
 
   acumulator *= numberToMultiply;
 
   outputResult(acumulator, description);
+  logCalculatorHistory(
+    '*',
+    numberToMultiply,
+    previousTotal,
+    acumulator,
+    description
+  );
 }
 
 function divide() {
   const numberToDivide = getUserInputAsInt();
+  const previousTotal = acumulator;
   let description;
 
   if (numberToDivide > 0) {
@@ -59,6 +104,13 @@ function divide() {
   }
 
   outputResult(acumulator, description);
+  logCalculatorHistory(
+    '/',
+    numberToDivide,
+    previousTotal,
+    acumulator,
+    description
+  );
 }
 
 addBtn.addEventListener('click', add);
